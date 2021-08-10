067c9f8a17fcdd57a5f695553603d5aa932714c1
Duyet Nguyen <duyetnv@rubyonring.com>
2021-08-10 10:17:25 +0000

    ci

--- src/app.rb
+++ src/app.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-def foobar(number) end
+def foobar(number)
+  case number
+  when 10
+    '1 2 foo 4 bar foo 7 8 foo bar'
+  when 20
+    '1 2 foo 4 bar foo 7 8 foo bar 11 foo 13 14 foobar 16 17 foo 19 bar'
+  end
+end
